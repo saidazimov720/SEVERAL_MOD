33707ee5cf6f68a75d1c5b814f9fc9ab9acfba97
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-23 16:12:08 +0500

    fixing

--- calculator_gui.py
+++ calculator_gui.py
@@ -9,7 +9,7 @@ class CalculatorGUI:
         self.create_widgets()
     
     def create_widgets(self):
-        entry = tk.Entry(self.master, textvariable=self.result_var, justify="right", font=('arial', 14))
+        entry = tk.Entry(self.master, textvariable=self.result_var, justify="right", font=('Arial', 14))
         entry.grid(row=0, column=0, columnspan=4, sticky="nsew")
         
         buttons = [
@@ -22,7 +22,8 @@ class CalculatorGUI:
         row_val, col_val = 1, 0
         for button in buttons:
             tk.Button(self.master, text=button, padx=20, pady=20, font=('Arial', 12),
-                      command=lambda btn=button:self.button_click(btn)).grid(row=row_val, column=col_val)
+            command=lambda btn=button:self.button_click(btn)).grid(row=row_val, column=col_val)
+            
             col_val +=1
             if col_val >3:
                 col_val = 0
