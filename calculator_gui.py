84dcd16a47124f36f617c751d3775a75f2fe61b8
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-23 15:39:43 +0500

    create_widgets

--- calculator_gui.py
+++ calculator_gui.py
@@ -7,3 +7,14 @@ class CalculatorGUI:
         master.title("calculator")
         self.result_var = tk.StringVar()
         self.create_widgets()
+    
+    def create_widgets(self):
+        entry = tk.Entry(self.master, textvariable=self.result_var, justify="right", from=("Arial", 14))
+        entry = grid(row=0, column=0, columspan=4, sticky="nsew")
+        
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', 'C', '=', '+'
+        ]
