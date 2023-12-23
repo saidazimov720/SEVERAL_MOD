fa9979b3f78443c71404144887a002f8bdeb2c18
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-23 15:35:57 +0500

    adding class CalculatorGUI

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 from calculator_logic import add, sub, mul, div
+
+class CalculatorGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("calculator")
+        self.result_var = tk.StringVar()
+        self.create_widgets()
